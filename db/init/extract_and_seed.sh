17ab7ec17f88be4d1445593d3929d61577690663
James Bescos <jamesbescos@gmail.com>
2025-02-11 23:42:34 -0800

    add step in init script to run views migration

--- db/init/extract_and_seed.sh
+++ db/init/extract_and_seed.sh
@@ -6,6 +6,9 @@ unzip -o /tmp/data.zip -d /tmp/
 echo "Creating tables..."
 psql -U "$POSTGRES_USER" -d "$POSTGRES_DB" -f /tmp/tables.sql
 
+echo "Creating views..."
+psql -U "$POSTGRES_USER" -d "$POSTGRES_DB" -f /tmp/views.sql
+
 echo "Seeding database..."
 psql -U "$POSTGRES_USER" -d "$POSTGRES_DB" -f /tmp/seed.sql
 
