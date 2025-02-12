e6d6268ade56452b88d9dccc92e82dc8dd93112e
James Bescos <jamesbescos@gmail.com>
2025-02-11 18:31:58 -0800

    add views migration with view for question 4

---
+++ db/migrations/views.sql
@@ -0,0 +1,62 @@
+-- Create Views
+
+/*
+    Question 1
+
+    Please write a query that shows all 2022 member (season ticket holder) account IDs and the unique sections that the member has seats in. 
+    There should be one row per account and member tickets are flagged with the plan event prefix 22FS.
+*/
+CREATE OR REPLACE VIEW vw_question_1 (
+);
+
+
+/*
+    Question 2 
+
+    Please write a query that aggregates total 2021 ticket spend, 2022 ticket spend and merchandise spend by account ID. 
+    There should be one row for each unique account ID.
+*/
+CREATE OR REPLACE VIEW vw_question_2 (
+);
+
+
+/*
+    Question 3 
+
+    Please write a query that shows the earliest purchase date for every account ID 
+    (the first purchase can be either from tickets or merchandise).
+*/
+CREATE OR REPLACE VIEW vw_question_3 (
+);
+
+
+/*
+    Question 4
+
+    Please write a query that shows the ticket sell through rate by section for the last match of the 2022 regular season 
+    (sell through rate = total sales/total manifest seats).
+*/
+CREATE OR REPLACE VIEW vw_question_4 (
+    WITH last_match AS (
+       SELECT event_id 
+       FROM events 
+       WHERE season_name = '2022 LAFC Season' 
+        AND minor_category = 'MLS SOCCER'
+       ORDER BY event_date DESC 
+       LIMIT 1
+    ),
+    sections AS (
+       SELECT
+           section_id,
+           SUM(COALESCE(num_seats, 0)) AS capacity
+       FROM manifests m
+       GROUP BY m.section_id
+    )
+    SELECT 
+        t.section_id,
+        COUNT(t.ticket_id) * 1.0 / NULLIF(s.capacity, 0) AS sell_through_rate
+    FROM tickets t
+    JOIN last_match lm ON t.event_id = lm.event_id
+    JOIN sections s ON t.section_id = s.section_id
+    GROUP BY t.section_id, s.capacity;
+);
