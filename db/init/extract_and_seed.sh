2429a6bb0a797ef5231b8fdc08b792cb0ea076d5
James Bescos <jamesbescos@gmail.com>
2025-02-12 18:37:05 -0800

    add step in init script to clean fnb csv

--- db/init/extract_and_seed.sh
+++ db/init/extract_and_seed.sh
@@ -3,6 +3,9 @@
 echo "Unzipping data..."
 unzip -o /tmp/data.zip -d /tmp/
 
+echo "Cleaning CSVs..."
+iconv -f WINDOWS-1252 -t UTF-8 /tmp/data/lafc_fb_data_sample.csv | sed 's/[^[:print:]\t]//g; s/\$//g' > /tmp/data/lafc_fb_data_sample_cleaned.csv
+
 echo "Creating tables..."
 psql -U "$POSTGRES_USER" -d "$POSTGRES_DB" -f /tmp/tables.sql
 
